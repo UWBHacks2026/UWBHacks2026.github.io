fed41a30883ec219e045e18a6439fd196ebadbca
Mr-Fishy <efishg2020@gmail.com>
2026-04-25 13:40:46 -0700

    Removed old DB Files

--- drizzle/0001_chilly_harrier.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `candidate_profiles` ADD `bio` text;--> statement-breakpoint
-ALTER TABLE `candidate_profiles` ADD `availability` text;--> statement-breakpoint
-ALTER TABLE `candidate_profiles` ADD `preferred_job_type` text;--> statement-breakpoint
-ALTER TABLE `employer_profiles` ADD `industry` text;--> statement-breakpoint
-ALTER TABLE `employer_profiles` ADD `website` text;--> statement-breakpoint
-ALTER TABLE `jobs` ADD `employment_type` text;
