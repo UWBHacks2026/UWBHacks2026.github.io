e3ba3d0f87f9e7724e6d1de1e69ac48374b2dcf0
Mr-Fishy <efishg2020@gmail.com>
2026-04-25 14:37:06 -0700

    Rendering "Correctly"

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -4,7 +4,12 @@ export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   dialect: "sqlite",
+  driver: "d1-http",
   dbCredentials: {
-    url: "./sqlite.db",
+    accountId: process.env.ACCOUNT_ID!,
+    databaseId: process.env.DATABASE_ID!,
+    token: process.env.CLOUDFLARE_API_TOKEN!,
   },
+  verbose: false,
+  strict: true,
 } satisfies Config;
